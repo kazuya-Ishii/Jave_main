057d0979b67bda92b4b08359254a4515d86efcc8
kazuk <kazuk@192.168.1.4>
2025-05-27 15:01:33 +0900

    コメント

--- HelloWorld_IshiiKazuya/src/basicClass/Animal.java
+++ HelloWorld_IshiiKazuya/src/basicClass/Animal.java
@@ -14,9 +14,13 @@ public class Animal {
         System.out.println("動物の数: " + dog2.number);
 
         // 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+        //現在の日時を取得。
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+       //指定の形式 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s"); 
+        //fomat()で整えた日時をformattedDateに代入
         String formattedDate = now.format(formatter);
+       // 日付/時間オブジェクトの出力
         System.out.println("現在の日時: " + formattedDate);
     }
 }
