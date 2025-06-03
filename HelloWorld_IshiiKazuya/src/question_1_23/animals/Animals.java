1b63210a8b3474da3fe0d710ec899f110896eb42
kazuk <kazuk@192.168.1.4>
2025-06-04 01:56:33 +0900

    test

--- HelloWorld_IshiiKazuya/src/question_1_23/animals/Animals.java
+++ HelloWorld_IshiiKazuya/src/question_1_23/animals/Animals.java
@@ -1,25 +1,29 @@
 package question_1_23.animals;
 
 	public class Animals {
+		//動物の名前
 	    private String name;
+	    //動物の体長
 	    private double length;
+	    //動物の速度
 	    private int speed;
 
-	    // セッター
+	    // セッター　値を設定する　メソッドでしかアクセスできない
 	    public void setName(String name) {
 	        this.name = name; // thisでフィールドを区別
 	    }
-
+        //double 小数を含む実数
 	    public void setLength(double length) {
 	        this.length = length;
 	    }
-
+	    //整数（小数なし）
 	    public void setSpeed(int speed) {
 	        this.speed = speed;
 	    }
 
-	    // ゲッター
+	    // ゲッター　値を取得する
 	    public String getName() {
+	    	//メソッドから値を返す
 	        return this.name;
 	    }
 
