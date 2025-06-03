8ede724a7f178b7087f8cb0f46a75d6e641310b3
kazuk <kazuk@192.168.1.4>
2025-06-03 19:34:07 +0900

    課題20

---
+++ HelloWorld_IshiiKazuya/src/curriculum20_22/logic/mainapp/Main.java
@@ -0,0 +1,8 @@
+package curriculum20_22.logic.mainapp;
+
+public class Main {
+    public static void main(String[] args) {
+        MessageLogic logic = new MessageLogic();  // インスタンス生成
+        logic.printMessages();                   // メッセージ表示
+    }
+}
